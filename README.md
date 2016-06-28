80ffba659e86692f13a42efd3022ea5805e6476a
Heiko Westermann <hwt3@gmx.de>
2016-06-28 18:46:16 +0200

    Version 0.0.3-beta: Updated meta data.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # HwtTypo3HwtSyscategories
-Install Note: Name the extension folder "hwt_syscategories"!
+###Install Note:  
+Name the extension folder "hwt_syscategories"!
 
-About: This TYPO3 extensions extends the sys_categories since TYPO3 6.x. It adds a category image and own model and repository for usage in custom extensions.
+###About:  
+This TYPO3 extensions extends the system categories since TYPO3 6.x.
+
+It adds an image and a link option to sys_category records. With an own model and repository it's prepared for usage in custom extbase extensions.
--- ext_emconf.php
+++ ext_emconf.php
@@ -29,10 +29,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.2',
+	'version' => '0.0.3',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.1.0-6.2.99',
+			'typo3' => '6.1.0-7.6.99',
 		),
 		'conflicts' => array(
 		),
@@ -41,5 +41,3 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"80be";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"d440";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"4688";s:20:"doc/wizard_form.html";s:4:"c8db";s:13:"mod1/conf.php";s:4:"691e";s:14:"mod1/index.php";s:4:"bdf5";s:18:"mod1/locallang.xml";s:4:"b5bc";s:22:"mod1/locallang_mod.xml";s:4:"db06";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
 );
-
-?>
