86330af0575f92e7a5125b6eb1f3a2f8df2b0ed4
tieupmedia <hwt3@gmx.de>
2017-10-04 18:21:21 +0200

    Version 0.0.4-beta: Tested in TYPO3 8.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,6 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'category' => 'plugin',
 	'author' => 'Heiko Westermann',
 	'author_email' => 'hwt3@gmx.de',
+	'author_company' => 'tie-up media',
 	'shy' => '',
 	'dependencies' => 'cms',
 	'conflicts' => '',
@@ -29,15 +30,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.3',
+	'version' => '0.0.4',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.1.0-7.6.99',
+			'typo3' => '6.1.0-8.7.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"80be";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"d440";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"4688";s:20:"doc/wizard_form.html";s:4:"c8db";s:13:"mod1/conf.php";s:4:"691e";s:14:"mod1/index.php";s:4:"bdf5";s:18:"mod1/locallang.xml";s:4:"b5bc";s:22:"mod1/locallang_mod.xml";s:4:"db06";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
 );
