46a717d182268464a5e517bfaabcb9f87306aaf9
Heiko Westermann <westermann@tieup-media.de>
2024-01-23 22:34:31 +0100

    [UPDATE] config: Replace extbase persistence mapping for TYPO3 >= 10.0, breaking #87623

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    \Hwt\HwtSyscategories\Domain\Model\Category::class => [
+        'tableName' => 'sys_category',
+    ],
+];
--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.5',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '9.0.0-9.5.99',
+			'typo3' => '10.0.0-10.4.99',
 		),
 		'conflicts' => array(
 		),
